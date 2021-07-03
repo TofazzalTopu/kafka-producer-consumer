077bafb7f5183dcd61a62c5c2cb7d97310c82062
Md.shamim Miah <shamim.ict0754@gmail.com>
2021-07-03 08:37:21 +0600

    password fixing

--- src/main/java/com/itc/notification/ServletInitializer.java
+++ src/main/java/com/itc/notification/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(IncentiveApplication.class);
+        return application.sources(NotificationApplication.class);
     }
 
 }
